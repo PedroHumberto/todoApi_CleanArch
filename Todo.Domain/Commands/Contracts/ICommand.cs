f8e0a8bcebd44f0b1dfc51fcaf55201267212784
Pedro Humberto <78572029+PedroHumberto@users.noreply.github.com>
2023-10-31 18:14:33 -0300

    first commit

---
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Todo.Domain.Commands.Contracts
+{
+    public interface ICommand
+    {
+        bool Validate();
+    }
+}
---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(string title, DateTime date, string user)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            FinishedAt = null;
+            User = user;
+        }
+
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public string User { get; private set; }
+
+        public void MarkAsDone()
+        {
+            FinishedAt = DateTime.UtcNow;
+            Done = true;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
